61084ffb3c0a52721dcedd2f3ad316bfbf85e95c
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-04-28 15:37:26 +0000

    updated

--- Assignment-thon-1.js
+++ Assignment-thon-1.js
@@ -3,34 +3,33 @@ let cars = {
   sedan : {
     price : 30,
     stock: 0,
-    isAvailable() {
-      if (this.stock > 0) return "Sedan: Available";
-      return "Sedan: Not Available";
-    }
   },
   
   suv : {
     price : 35,
-    stock : 10,
-    isAvailable() {
-      if (this.stock > 0) return "suv: Available";
-      return "suv: Not Available";
-    }
+    stock : 10
   },
   
   sports : {
     price : 40,
     stock : 5,
-    isAvailable() {
-      if (this.stock > 0) return "sports: Available";
+    
+  },
+  
+  isAvailable(car) {
+    if(this[car]) {
+      if (this[car].stock > 0) return "sports: Available";
       return "sports: Not Available";
+    }
+    else {
+      return "Not a car";
     }
   },
   
   rent(car){
     if(this[car]){
       this[car].stock --;
-      return car, "rented";
+      return `${car} rented`;
     } else {
       return "not a car";
     }
@@ -38,6 +37,7 @@ let cars = {
   
 };
 
+console.log(cars.isAvailable('sports'));
 console.log(cars.sports.stock);
 console.log(cars.rent('sports'));
 console.log(cars.sports.stock);
